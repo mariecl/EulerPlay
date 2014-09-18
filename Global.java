394e798b5432d1b686fb236a0822ce540f58a37f
Marie Clemessy <marie.clemessy@gmail.com>
2014-09-18 14:40:33 -0400

    Added constraints to form + Added error pages

---
+++ Global.java
@@ -0,0 +1,27 @@
+import play.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import play.libs.F.*;
+
+import static play.mvc.Results.*;
+
+public class Global extends GlobalSettings {
+
+    public Promise<Result> onError(RequestHeader request, Throwable t) {
+        return Promise.<Result>pure(internalServerError(
+                views.html.errorPage.render(t)
+        ));
+    }
+
+    public Promise<Result> onHandlerNotFound(RequestHeader request) {
+        return Promise.<Result>pure(notFound(
+                views.html.notFoundPage.render(request.uri())
+        ));
+    }
+
+    public Promise<Result> onBadRequest(RequestHeader request, String error) {
+        return Promise.<Result>pure(badRequest("Don't try to hack the URI!"));
+    }
+
+
+}
